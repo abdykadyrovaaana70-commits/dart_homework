45189860dcc37c8902bd3c2279cb86aab3039108
abdykadyrovaaana70-commits <abdykadyrovaaana70@gmail.com>
2025-12-13 17:39:06 +0600

    Add files via upload

---
+++ lib/profileApp.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/profilePage.dart';
+class ProfileApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'User Profile',
+     home: ProfilePage()
+  
+     );
+  }
+}
